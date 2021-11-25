1fb54568023b97c1c00c4286ccf7310e4b6fac58
nicol <nicol@PC_Materiel_net>
2021-11-25 18:44:12 +0100

    Mise à jour de la cible java pour création d'un jar executable. corection de typo.

--- problem/src/problem/ListeRandomTest.java
+++ problem/src/problem/ListeRandomTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 class ListeRandomTest {
 
